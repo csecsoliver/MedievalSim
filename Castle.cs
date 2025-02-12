aa2793b72d5d5569d0ce1d2bdb646c84590f1aa6
Csécs Olivér Elemér <0oliogamer0@gmail.com>
2025-02-12 10:25:20 +0100

    King and Castle class

--- Castle.cs
+++ Castle.cs
@@ -2,4 +2,11 @@ namespace MedievalSim;
 
 public class Castle
 {
+    public string Name { get; protected set; }
+    public Citizen Captain { get; protected set; }
+    public Castle(string name, Citizen captain,){
+        Name = name;
+        Captain = captain;
+    }
+
 }
--- King.cs
+++ King.cs
@@ -2,6 +2,7 @@ namespace MedievalSim;
 
 public class King : Citizen
 {
+    public
     public King(string name, double wealth, int age, Kingdom kingdom) : base(name, wealth, age, kingdom)
     {
     }
