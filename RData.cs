f70159b0adf051a43b48b65eeccb64ccf09dac6c
csecsoliver <csecs.oliver.elemer@students.jedlik.eu>
2025-02-14 14:35:37 +0100

    Collection of Citizen, King and Kingdom names

---
+++ RData.cs
@@ -0,0 +1,59 @@
+namespace MedievalSim;
+
+public static class RData
+{
+    public static readonly string[] KingNames =
+    {
+        "Arthur", "Henry", "Edward", "Richard", "Louis", "George", "James", "Philip", "William", "Frederick",
+        "Charles", "Robert", "Alexander", "John", "David", "Oscar", "Cyrus", "Solomon", "Otto", "Sigismund",
+        "Clovis", "Harold", "Julius", "Constantine", "Theodore", "Balthazar", "Elias", "Gustav", "Afonso",
+        "Maximilian", "Napoleon", "Maharaja", "Tamerlane", "Suleiman", "Leopold", "Ferdinand", "Nicholas",
+        "Michael", "Ivan", "Vladimir", "Augustus", "Christian", "Alfonso", "Casimir", "Stephen", "Bogdan", "Pedro",
+        "Joao", "Boris", "Simeon", "Matthias", "Sigurd", "Canute", "Haakon", "Eystein", "Magnus", "Eric", "Birger",
+        "Carl", "Olaf", "Edgar", "Edmund", "Aethelred", "Sweyn", "Harthacnut", "Malcolm", "Kenneth", "Duncan",
+        "Donald", "Alexander", "Brian", "Roderic", "Dermot", "Toirdelbach", "Niall", "Ruaidrí", "Llywelyn", "Owain",
+        "Rhys", "Griffith", "Wenceslaus", "Vladislav", "Ladislaus", "Geza", "Bela", "Stephen", "Mindaugas",
+        "Vytautas", "Gediminas", "Algirdas", "Basil", "Alexios", "Manuel", "Andronikos", "Romanos", "Nikephoros",
+        "Zeno", "Arcadius", "Theodosius", "Justinian"
+    };
+    public static readonly string[] Names =
+    {
+        "Alaric", "Aldric", "Aldwin", "Amedeo", "Anselm", "Arnold", "Aubert", "Aymar", "Baldwin", "Bartholomew",
+        "Berengar", "Bertrand", "Benoit", "Bernard", "Blanche", "Bonaventure", "Boris", "Bonaventura", "Calixtus", "Catherine",
+        "Charles", "Chretien", "Claudia", "Conrad", "Cyprian", "Damien", "Dante", "David", "Denis", "Desiderius",
+        "Dominic", "Egon", "Emery", "Enrico", "Ethelred", "Eudora", "Eustace", "Evangeline", "Fabian", "Faramund",
+        "Felix", "Ferdinand", "Geraldo", "Gilbert", "Godfrey", "Gontran", "Gregory", "Guillaume", "Gustav", "Hedwig",
+        "Henry", "Hermann", "Hildegard", "Hubert", "Ignatius", "Isidore", "Jacques", "Jasper", "Jeremiah", "Jocelyn",
+        "Joffrey", "Joseph", "Julius", "Julian", "Lancelot", "Leopold", "Louis", "Lucian", "Malcolm", "Martin",
+        "Matilda", "Maurice", "Maximilian", "Melisende", "Michael", "Miles", "Milo", "Nicolas", "Norbert", "Odilon",
+        "Oliver", "Oswald", "Pascal", "Patrick", "Peregrine", "Philip", "Piers", "Raimund", "Ralph", "Reginald",
+        "Reinhard", "Richard", "Robert", "Roland", "Roderic", "Roger", "Rudolf", "Salvator", "Samuel", "Siegfried",
+        "Simeon", "Sylvester", "Tancred", "Theobald", "Theodora", "Thomas", "Théodore", "Tobias", "Ulf", "Ursula",
+        "Valentin", "Valeria", "Vincenzo", "Vincent", "Vladimir", "Walter", "Wilhelm", "Willem", "William", "Wolfgang",
+        "Zacharias", "Zeno", "Abelard", "Aedric", "Agnes", "Alban", "Alistair", "Aloysius", "Amandus", "Amadeus",
+        "Anastasia", "Antoine", "Arianne", "Aubrey", "Augustin", "Aurelia", "Benedict", "Benedetta", "Berthold", "Bleddyn",
+        "Bonaventure", "Bruno", "Caleb", "Cecilia", "Celeste", "Clement", "Constantine", "Cornelia", "Cyra", "Damaris",
+        "Damien", "Darcy", "David", "Delphine", "Demetrius", "Desiderius", "Donatienne", "Duncan", "Durand", "Eleanor",
+        "Elias", "Eloise", "Emmanuel", "Epiphanius", "Esme", "Eugene", "Eva", "Fergus", "Ferdinand", "Florence",
+        "Fortunato", "Francis", "Frederick", "Geoffrey", "Giselle", "Guinevere", "Gustavus", "Hilda", "Ingrid", "Isabelle",
+        "Isidore", "Jade", "Jeremiah", "Joanna", "John", "Josephine", "Juliette", "Jules", "Katherine", "Lucian",
+        "Lysandra", "Madeline", "Malcolm", "Manuel", "Marcella", "Marianne", "Maximus", "Melisande", "Meredith", "Miles",
+        "Miriam", "Narcissa", "Natalia", "Nicholas", "Olivia", "Owen", "Pascal", "Patricia", "Pierre", "Quentin",
+        "Raimundo", "Raoul", "Raymond", "Roderic", "Roland", "Rosalie", "Rosalind", "Sabrina", "Sebastian", "Simone",
+        "Sophie", "Sylvester", "Talon", "Theodora", "Theresa", "Thibault", "Thomasina", "Ursula", "Victor", "Vivienne",
+        "Wilfred", "Winston", "Xavier", "Yseult", "Zachary", "Zora", "Adelaide", "Alfie", "Alistair", "Amelia",
+        "Basil", "Beatrice", "Beatrix", "Bertha", "Benedicta", "Berthold", "Bridget", "Celia", "Clara", "Edmund",
+        "Elena", "Eustace", "Frederica", "Gerard", "Gilbert", "Gwyneth", "Hannah", "Hugo", "Isabella", "Jasper",
+        "Josephine", "Julian", "Katharina", "Lysbeth", "Maurice", "Melisandra", "Matilda", "Peter", "Ralph", "Rupert"
+    };
+    public static readonly string[] KingdomNames =
+    {
+        "Avalon", "Camelot", "Rohan", "Gondor", "Westeros", "Asgard", "Valyria", "Narnia", "Hogwarts", "Elbonia",
+        "Lothlórien", "Mordor", "Albion", "Fiefdom", "Atlantis", "Zakhara", "Jotunheim", "Shire", "Midgard", "Eldoria",
+        "Arendelle", "Erendorn", "Rivendell", "Dorne", "Kalmar", "Vikingar", "Scandia", "Brittania", "Vernor", "Baronia",
+        "Thaloria", "Khazad-dûm", "Belkora", "Angmar", "Axelberg", "Normandy", "Pyrene", "Ithil", "Sarovia", "Eryndor",
+        "Tirith", "Soria", "Alcarin", "Isla de la Luna", "Nordheim", "Tauriel", "Erithria", "Desmarche", "Valkyria",
+        "Sarnath", "Ostmark", "Lunaris", "Altair", "Eriador", "Elandor"
+    };
+    
+}
